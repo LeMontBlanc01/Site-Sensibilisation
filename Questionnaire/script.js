6ac16552b355d80d35d0636788cab41647f25fa2
Brando <brando.colamarco@stfelixlasalle.fr>
2026-05-20 09:16:47 +0200

    Update script.js

--- Questionnaire/script.js
+++ Questionnaire/script.js
@@ -1,11 +1,14 @@
-console.log("JS chargé !");
+console.log("JS chargé !"); // Affiche un message dans la console pour confirmer que le fichier JS est bien chargé
 
-document.getElementById("btn-valider-q2").style.display = "none";
+document.getElementById("btn-valider-q2").style.display = "none"; //Cache le bouton de validation de la question 2 au chargement
+
+//Fonction qui affiche une question et cache les autres
 function showQuestion(id) {
-  document.querySelectorAll('.question').forEach(q => q.style.display = 'none');
-  document.getElementById(id).style.display = 'block';
-  
 
+  document.querySelectorAll('.question').forEach(q => q.style.display = 'none');  //On récupère toutes les divs de la classe "question" et on les cache
+  document.getElementById(id).style.display = 'block';  //On affiche uniquement la question dont l'id est passé en paramètre
+  
+  //Si on change de question, on efface les résultats de Q1
   if (id !== 'q1') {
     const result1 = document.getElementById('result-q1');
     if (result1) {
@@ -13,6 +16,7 @@ function showQuestion(id) {
     }
   }
 
+  //Pour Q2
   if (id !== 'q2') {
     const result2 = document.getElementById('result-q2');
     if (result2) {
@@ -20,6 +24,7 @@ function showQuestion(id) {
     }
   }
 
+  //Pour Q3
   if (id !== 'q3') {
     const result3 = document.getElementById('result-q3');
     if (result3) {
@@ -28,18 +33,20 @@ function showQuestion(id) {
   }
 }
 
+//Fonction qui permet de vérifier la réponse de la question 1
 function checkQ1() {
-    const selected = Array.from(document.querySelectorAll('input[name="q1"]:checked'))
+    const selected = Array.from(document.querySelectorAll('input[name="q1"]:checked'))  //Récupère toutes les cases cochées de la question 1 et on prend leur valeur
                           .map(el => el.value);
 
-    const correct = ["g7!pl9@vq2#rt"];
+    const correct = ["g7!pl9@vq2#rt"]; //La bonne réponse
 
-    const result = document.getElementById("result-q1");
+    const result = document.getElementById("result-q1");  //Zone où afficher le résultat
 
+    //Bonne réponse ou mauvaise réponse selon les cases cochées
     if (arraysEqual(selected, correct)) {
         result.textContent = "Bonne réponse ! ";
         result.style.color = "green";
-        document.getElementById("btn-valider-q1").style.display = "none";
+        document.getElementById("btn-valider-q1").style.display = "none"; //Cacher le bouton de validation une fois que c'est validé
     } else {
         result.textContent = "Mauvaise réponse.";
         result.style.color = "red";
