2d1f269a8c9beaec14b7abf1bdc6b3c40f4e39c4
Elric <elric.evrard@stfelixlasalle.fr>
2026-05-20 11:41:49 +0200

    ajout du point sur la question 1

--- Questionnaire/script.js
+++ Questionnaire/script.js
@@ -145,11 +145,13 @@ function checkQ1() {
     if (arraysEqual(selected, correct)) {
         result.textContent = "Bonne réponse ! Un bon mot de passe doit contenir au moins 12 caractères, avec une combinaison de lettres majuscules et minuscules, de chiffres et de caractères spéciaux.";
         result.style.color = "green";
+        score ++;
         document.getElementById("btn-valider-q1").style.display = "none";
         document.getElementById("btn-suivant-q1").style.display = "inline-block";
     } else {
         result.textContent = "Mauvaise réponse.";
         result.style.color = "red";
+        score --;
     }
 }
 
