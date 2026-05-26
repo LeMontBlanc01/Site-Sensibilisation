222b386024fa515b483c92baf6f833e58664b321
Brando <brando.colamarco@stfelixlasalle.fr>
2026-05-26 16:37:35 +0200

    test

--- Questionnaire/script.js
+++ Questionnaire/script.js
@@ -29,10 +29,15 @@ function initRandomQuestions() {
         nextBtn.textContent = 'Voir les résultats';
       }
     }
+    
+    const h2 = document.querySelector(`#${id} h2`);
+    if (h2) h2.textContent = `Question ${idx + 1}`;
+
   });
 
   document.querySelectorAll('.question').forEach(q => q.style.display = 'none');
   document.getElementById(questionOrder[0]).style.display = 'block';
+  melangerReponses(questionOrder[0]);
 }
 
 function melangerReponses(questionId) {
