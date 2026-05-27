a60adefd675c5f6a16ad2ddad902fac9de69167b
Brando <brando.colamarco@stfelixlasalle.fr>
2026-05-27 15:02:00 +0200

    modiff q151617 mid test

--- Questionnaire/script-moyen.js
+++ Questionnaire/script-moyen.js
@@ -498,8 +498,8 @@ function checkQ14() {
 
 //Bonnes réponses : clé = bloc gauche (A/B/C), valeur = bloc droit attendu (1/2/3)
 const configs = {
-  15: { correctMap: { A: "3", B: "1", C: "2" } },
-  16: { correctMap: { A: "2", B: "1", C: "3" } },
+  15: { correctMap: { A: "1", B: "2", C: "3" } },
+  16: { correctMap: { A: "3", B: "2", C: "1" } },
   17: { correctMap: { A: "2", B: "1", C: "3" } },
 };
 
@@ -507,15 +507,15 @@ const configs = {
 //Appelée quand toutes les connexions d'une question sont correctes
 //À compléter avec une explication personnalisée par question
 function onBonneReponse15(result) {
-  result.innerHTML = "Bonne réponse !<br><small>Si l'adresse de messagerie a été usurpée, il faut immédiatement changer le mot de passe pour éviter que l'attaquant ne conserve l'accès au compte et ne continue à envoyer des messages frauduleux en votre nom. Si, par erreur, vous communiquez votre numéro de carte bancaire vous devez faire opposition auprès de votre banque et déposer plainte. Si vous identifiez une adresse de site d'hameçonnage (site qui peut voler des identifiants, infecter le système ou accéder au réseau) vous devez le signaler à Phishing Initiative (Plateforme de signalement et de prévention contre l'hameçonnage).</small>";
+  result.innerHTML = "Bonne réponse !<br><small>Si</small>";
 }
 
 function onBonneReponse16(result) {
-  result.innerHTML = "Bonne réponse !<br><small>Si vous travaillez régulièrement à l'extérieur, évitez de vous connecter à un réseau Wi-Fi public, car ces réseaux sont souvent non sécurisés et peuvent permettre à des personnes malveillantes d'intercepter vos données. Si vous perdez ou vous vous faites voler votre téléphone, vous devez bloquer votre ligne en appelant votre opérateur et bloquer votre téléphone en communiquant votre code IMEI (identifiant unique de la puce réseau de votre appareil), puis déposer plainte. Si vous téléchargez un jeu sur votre téléphone, n'autorisez pas l'accès à vos photos, vos contacts et vos messages, car un jeu n'a aucune raison légitime d'accéder à ces données personnelles.</small>";
+  result.innerHTML = "Bonne réponse !<br><small>Si </small>";
 }
 
 function onBonneReponse17(result) {
-  result.innerHTML = "Bonne réponse !<br><small>Si vous êtes à la maison et vous devez consulter vos messages professionnels, assurez-vous de le faire uniquement à partir de votre ordinateur professionnel. Si vous vous apprêtez à stocker des documents professionnels sur un service en ligne personnel, demandez l'autorisation à votre employeur et prenez des mesures de sécurité supplémentaires. Si ça vous arrive de réaliser des téléchargements illégaux depuis votre ordinateur professionnel, votre entreprise pourrait contrôler votre utilisation de la connexion Internet professionnelle et se retourner contre vous.</small>";
+  result.innerHTML = "Bonne réponse !<br><small>Si </small>";
 }
 
 //Regroupe les callbacks par numéro de question pour les appeler dans la boucle
